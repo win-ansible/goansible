ef7d3b6ed995cb45dc33fe3852da31d69ba1a017
Roman Belyakovsky <ihryamzik@gmail.com>
2017-01-31 16:05:05 +0100

    Cleaned up unsed code

--- goansible.go
+++ goansible.go
@@ -7,9 +7,7 @@ import (
 	. "github.com/CodyGuo/win"
 	"github.com/kardianos/osext"
 	"golang.org/x/sys/windows"
-	"io"
 	"log"
-	"net/http"
 	"os"
 	"os/exec"
 	"regexp"
@@ -40,31 +38,6 @@ func CheckErrorFatal(err error) {
 		reader.ReadString('\n')
 		log.Panic(err)
 	}
-}
-
-func downloadFile(filepath string, url string) (err error) {
-
-	// Create the file
-	out, err := os.Create(filepath)
-	if err != nil {
-		return err
-	}
-	defer out.Close()
-
-	// Get the data
-	resp, err := http.Get(url)
-	if err != nil {
-		return err
-	}
-	defer resp.Body.Close()
-
-	// Writer the body to file
-	_, err = io.Copy(out, resp.Body)
-	if err != nil {
-		return err
-	}
-
-	return nil
 }
 
 func setTempDir() {
